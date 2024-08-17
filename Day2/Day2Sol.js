5fa938ffae189660a7c93c0fda158ffe13bd2014
yashPundhir <yash.pundhir.prof@gmail.com>
2024-08-17 13:50:42 +0530

    Day 2 challenge done

---
+++ Day2/Day2Sol.js
@@ -0,0 +1,111 @@
+// Day 2: Operators
+
+// Activity 1 => Task 1
+
+let num1 = 12;
+let num2 = 4;
+console.log(num1 + num2);
+
+// Activity 1 => Task 2
+
+console.log(num1 - num2);
+
+// Activity 1 => Task 3
+
+console.log(num1 * num2);
+
+// Activity 1 => Task 4
+
+console.log(num1 / num2);
+
+// Activity 1 => Task 5
+
+console.log(num1 % num2);
+
+// Activity 2 => Task 6
+
+console.log((num1 += 10));
+
+// Activity 2 => Task 7
+
+console.log((num2 -= 2));
+
+// Activity 3 => Task 8
+
+console.log(10 > 8);
+
+console.log(10 < 8);
+
+// Activity 3 => Task 9
+
+console.log(10 >= 10);
+
+console.log(10 <= 20);
+
+// Activity 3 => Task 10
+
+console.log(10 == "10");
+
+console.log(10 === "10");
+
+// Activity 4 => Task 11
+
+console.log(10 > 20 && 10 < 4);
+
+// Activity 4 => Task 12
+
+console.log(10 < 20 || 10 < 4);
+
+// Activity 4 => Task 13
+
+console.log(!(10 > 20));
+
+// Activity 5 => Task 14
+
+console.log(num1 > 0 ? "number is positive" : "number is negative");
+
+// Feature Request 1 => Arithmetic Operations Script
+
+let num3 = 7;
+
+let num4 = 8;
+
+console.log(num3 + num4);
+
+console.log(num3 - num4);
+
+console.log(num3 * num4);
+
+console.log(num3 / num4);
+
+console.log(num3 % num4);
+
+// Feature Request 2 => Comparison and Logical Operators Script
+
+let num5 = 20;
+
+let num6 = 22;
+
+console.log(num5 < num6);
+
+console.log(num5 > num6);
+
+console.log(num5 >= num6);
+
+console.log(num5 <= num6);
+
+console.log(num5 == num6);
+
+console.log(num5 === num6);
+
+console.log(num5 <= num6 && num5 < num6);
+
+console.log(num5 > num6 || num5 < num6);
+
+console.log(!(num5 > num6));
+
+// Feature Request 2 => Comparison and Logical Operators Script
+
+let result = -10 > 0 ? "number is positive" : "number is negative";
+
+console.log(result);
